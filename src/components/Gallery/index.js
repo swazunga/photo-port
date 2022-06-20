804ccb09cb88700240952b200721a64a8a36bf7b
Michael Kendrick <michael@locumsmart.net>
2022-06-20 12:52:17 -0500

    middle of 3

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import photo from "../../assets/small/commercial/0.jpg";
+
+function Gallery(props) {
+  const currentCategory = {
+    name: "commerical",
+    description:
+      "Photos of grocery stores, food trucks, and other commerical projects",
+  };
+  return (
+    <section>
+      <h1> {capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p> {currentCategory.name}</p>
+      <div>
+        <img
+          src={photo}
+          alt="Commercial Example"
+          className="img-thumbnail mx-1"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
