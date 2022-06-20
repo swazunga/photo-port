a1a6d9b2375ba6d14a5988cbb4c6039e3c8fb59a
Michael Kendrick <michael@locumsmart.net>
2022-06-19 21:06:43 -0500

    getting help

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
   const categories = [
